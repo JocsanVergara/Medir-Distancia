11fc0ac3ecaa5d8e8719cbd42b028a8e4f9ed156
Jocsan Vergara Blanco <jocsanvergarab@gmail.com>
2022-06-01 18:58:20 -0400

    Se omite el 1er valor, y se obtiene la info

--- Comunicacion_Serial.py
+++ Comunicacion_Serial.py
@@ -15,9 +15,33 @@ U_Blox = serial.Serial(str(Puerto_COM),115200, timeout=2, write_timeout=1)
 
 try:
     while(True):
+        
         dato = U_Blox.readline()
-        dato = dato.decode('utf-8')
-        print(dato)
+        dato_string = dato.decode('utf-8')
+        #dato_string = dato
+        #dato_string = ''.join([str(item) for item in dato])
+        print("Conversion de lista a string \n")
+        print(dato_string)
+
+
+        if ":CCF957966AC9" in dato_string:
+        #Separamos los datos entregados por el U-Blox
+            x = dato_string.split(",")
+            RSSI_1p = x[1]
+            Azimuth_angle = x[2]
+            Elevation_angle = x[3]
+            RSSI_2p = x[4]
+            Adv_Channel = x[5]
+        
+        #     #Vemos los datos
+            print(x)
+            #print(dato)
+            print("RSSI_1p:",RSSI_1p)
+            print("Azimuth_angle:",Azimuth_angle)
+            print("Elevation_angle:",Elevation_angle)
+            print("RSSI_2p:",RSSI_2p)
+            print("Adv_Channel:",Adv_Channel)
+
 except KeyboardInterrupt:
     U_Blox.close()
     print("Fin de la recolección de datos")
